35d3a6515283d137aae2f43a064d73e5cf107597
johnmuthua <caveomuthama@gmail.com>
2023-11-22 21:39:03 +0300

    Add maincontent

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import MainContent from "@/components/MainContent";
 import MainSidebar from "@/components/MainSidebar";
 import MinorSidebar from "@/components/MinorSidebar";
 export default function Home() {
   return (
-    <main className="h-[100vh] flex w-[100vw] border-2 border-green-300 bg-white">
+    <main className="h-[100vh] flex w-[100vw] bg-white">
       <MainSidebar />
       <MinorSidebar />
+      <MainContent />
     </main>
   );
 }
--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import { LuClipboardEdit } from "react-icons/lu";
 const MainContent = () => {
-  return <div>MainContent</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <div>
+            <h1 className="text-lg font-bold">
+              Add Agent Seats to your Subscription
+            </h1>
+          </div>
+          <div className="flex flex-row">
+            <span className="rounded-md bg-blue-400 text-white mx-2">
+              Published
+            </span>
+            <span> Last edited 1 day ago by</span>
+            <span>Santi Cazorla</span>
+          </div>
+        </div>
+        <div>
+          <LuClipboardEdit className="text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainContent;
