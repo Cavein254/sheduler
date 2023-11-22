9e268f5455c3fcc29b9b9c8156d77d1b6c363752
johnmuthua <caveomuthama@gmail.com>
2023-11-23 00:04:21 +0300

    Add searchbar

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MinorSidebar from "./MinorSidebar";
 import MainContent from "./MainContent";
-
+import { FaEllipsis } from "react-icons/fa6";
 const Main = () => {
   return (
     <article className="flex flex-col px-4 pt-2 w-full h-[100vh]">
-      <div>
+      <div className="flex justify-between items-center">
         <h1 className="text-3xl font-bold p-4">Knowledge Base</h1>
+        <FaEllipsis />
       </div>
       <hr className="border-[1px] border-gray-300  -mx-4" />
       <div className="flex flex-row">
